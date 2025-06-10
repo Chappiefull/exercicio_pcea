cc6d51f51966400e3f0c72a97685d9156cc077fe
Chappiefull <chappiefull@gmail.com>
2025-06-09 22:43:47 -0300

    Update soma.py
    - Mudando soma para função lambda
    - Adicionando as funções de subtração, multiplicação, divisão, potência e radiciação usando função lambda
    -Adicionando comentários explicando cada função

--- soma.py
+++ soma.py
@@ -1,2 +1,6 @@
-def soma(e,b):
-    return e+b
+soma = lambda n1, n2: n1 + n2 #Função que faz a soma entre dois números
+sub = lambda n1, n2: n1 - n2 #Função que faz a subtração entre dois números
+mult = lambda n1, n2: n1 * n2 #Função que faz a multiplicação entre dois números
+div = lambda n1, n2: n1 / n2 #Função que faz a divisão entre dois números
+pot = lambda n1, n2: n1 ** n2 #Função que faz a potência entre dois números (n1 é a base e n2 é o expoente)
+raiz = lambda n1, n2: n1 ** (1/n2) #Função que faz a radiciação (n1 é o radicando e n2 é o índice)
